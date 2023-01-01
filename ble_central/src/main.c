177c0aa5b907403122cad30130ddf80e53566418
jake-is-ESD-protected <jts4681@students.fhv.at>
2023-01-01 20:18:38 +0100

    minimal working example, cleanup in progress

--- ble_central/src/main.c
+++ ble_central/src/main.c
@@ -7,81 +7,11 @@
 #include "host/util/util.h"
 #include "console/console.h"
 #include "services/gap/ble_svc_gap.h"
-#include "blecent.h"
 #include "freertos/task.h"
 
-#define ESP1_PEER_ADDR "84:F7:03:09:0B:DA"
-
-static const char *tag = "NimBLE_BLE_CENT";
-static int blecent_gap_event(struct ble_gap_event *event, void *arg);
-
-static void blecent_scan(void)
-{
-    uint8_t own_addr_type;
-    struct ble_gap_disc_params disc_params;
-    int rc;
-
-    /* Figure out address to use while fprint_adv_fieldstising (no privacy for now) */
-    rc = ble_hs_id_infer_auto(0, &own_addr_type);
-    if (rc != 0)
-    {
-        MODLOG_DFLT(ERROR, "error determining address type; rc=%d\n", rc);
-        return;
-    }
-
-    disc_params.filter_duplicates = 1;
-    disc_params.passive = 1;
-    disc_params.itvl = 0;
-    disc_params.window = 0;
-    disc_params.filter_policy = 0;
-    disc_params.limited = 0;
-
-    rc = ble_gap_disc(own_addr_type, BLE_HS_FOREVER, &disc_params,
-                      blecent_gap_event, NULL);
-    if (rc != 0)
-    {
-        MODLOG_DFLT(ERROR, "Error initiating GAP discovery procedure; rc=%d\n",
-                    rc);
-    }
-}
-
-static int blecent_gap_event(struct ble_gap_event *event, void *arg)
-{
-    struct ble_hs_adv_fields fields;
-    int rc;
-
-    rc = ble_hs_adv_parse_fields(&fields, event->disc.data,
-                                 event->disc.length_data);
-    if (rc != 0)
-    {
-        return 0;
-    }
-
-    // if adv ESP was identified (69 as unique dummy value)
-    if (fields.tx_pwr_lvl == 69)
-    {
-        MODLOG_DFLT(INFO, "[DEBUG]: Trying to print advertisements:\n");
-        print_adv_fields(&fields);
-        MODLOG_DFLT(INFO, "[DEBUG]: RSSI %d:\n", event->disc.rssi);
-    }
-    return 0;
-}
-
-static void blecent_on_sync(void)
-{
-    int rc;
-    rc = ble_hs_util_ensure_addr(0);
-    assert(rc == 0);
-    blecent_scan();
-}
-
-void blecent_host_task(void *param)
-{
-    ESP_LOGI(tag, "BLE Host Task Started");
-    nimble_port_run();
-
-    nimble_port_freertos_deinit();
-}
+#include "blecent.h"
+#include "callbacks.h"
+#include "constants.h"
 
 void app_main(void)
 {
